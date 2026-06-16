a69f22df9d4d06244ca4441ef9e0335740fd4011
KarlqvistLars <le.karlqvist@gmail.com>
2026-06-16 16:08:03 +0200

    MovieAPIContext flyttad till Data

--- MovieAPI/Data/MovieAPIContext.cs
+++ MovieAPI/Data/MovieAPIContext.cs
@@ -1,5 +1,7 @@
 using Microsoft.EntityFrameworkCore;
 
+namespace MovieAPI.Data;
+
 public class MovieAPIContext(DbContextOptions<MovieAPIContext> options) : DbContext(options)
 {
     public DbSet<MovieAPI.Models.Movie> Movies { get; set; } = default!;
